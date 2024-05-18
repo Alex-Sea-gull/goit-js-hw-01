ab8611109dd331dd4a3b1af858373191faa157c9
Alex Sea <seagullalex81@gmail.com>
2024-05-18 15:07:44 +0300

    solution first task js

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction (orderedQuantity, pricePerItem) {
+    let quantity = orderedQuantity;
+    let totalPrice = orderedQuantity * pricePerItem;
+    console.log("You ordered ${ quantity } droids worth ${ totalPrice } credits!")
+    return `You ordered ${ quantity } droids worth ${ totalPrice } credits!`
+}
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!
